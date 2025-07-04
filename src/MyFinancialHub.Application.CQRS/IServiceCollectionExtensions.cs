9d6da586b5a2655374db458ce18f49855426a069
Franklin Bitencourt <Franklin.Bdantas@hotmail.com>
2025-07-04 10:34:35 -0300

    Adds Dispatcher

---
+++ src/MyFinancialHub.Application.CQRS/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MyFinancialHub.Application.CQRS.Handlers.Dispatchers;
+
+namespace MyFinancialHub.Application.CQRS
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCqrs(this IServiceCollection services)
+        {
+            return services
+                .AddScoped<IDispatcher, Dispatcher>()
+                .AddHandlers();
+        }
+
+        private static IServiceCollection AddHandlers(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
